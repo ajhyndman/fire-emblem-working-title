0c6fca660d317f99c345a27a9e761ab6a013d988
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-04 15:37:00 +1100

    Scrape for skill data
    Fixes #1

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+/* eslint-disable no-console */
 import fs from 'fs';
 import fetch from 'isomorphic-fetch';
 import {
-  head,
+  compose,
+  filter,
+  flatten,
+  isNil,
+  map,
+  match,
+  merge,
   mergeAll,
+  mergeWith,
+  not,
+  replace,
   tail,
+  trim,
+  zipObj,
 } from 'ramda';
 
-const parseHeroRow = (heroRow) => {
+
+/**
+ * HTML parsers
+ */
+
+const parseHeroRowHtml = (heroRow) => {
   const [
     colTitle,
     colMoveType,
@@ -40,13 +57,99 @@ const parseHeroRow = (heroRow) => {
       total,
     },
   };
+};
+
+const parseHeroAggregateHtml = (heroAggregateHtml) => {
+  const heroRows = match(/<tr>((.|\n)*?)<\/tr>/g, heroAggregateHtml);
+  return mergeAll(tail(heroRows).map(parseHeroRowHtml));
+};
+
+const parseHeroDetailHtml = (heroDetailHtml) => {
+  const heroSkillTables = filter(
+    compose(not, isNil),
+    match(/<table.*?skills-table.*?>(.|\n)*?<\/table>/g, heroDetailHtml)
+  );
+
+  // console.log(heroSkillTables);
+  const heroSkills = compose(
+    filter(compose(not, isNil)),
+    flatten,
+    map(compose(
+      map(compose(
+        trim,
+        replace(/<\/a>/g, ''),
+        replace(/<a.*?>/g, '')
+      )),
+      match(/<a.*?>.*?<\/a>/g)
+    ))
+  )(heroSkillTables);
+
+  return heroSkills;
+};
+
+
+/**
+ * Raw data fetchers
+ */
+
+const fetchAggregateStats = () =>
+  fetch('http://feheroes.wiki/Stats_Table')
+    .then(response => response.text());
+
+const fetchDetailStats = (heroName) =>
+  fetch(`http://feheroes.wiki/${encodeURIComponent(heroName)}`)
+    .then(response => {
+      if (!response.ok) return Promise.reject({ type: '404' });
+      // console.log('response status', `http://feheroes.wiki/${encodeURIComponent(heroName)}`, response.status);
+      return response.text();
+    })
+    .catch(() => console.error('failed to fetch: ', `http://feheroes.wiki/${encodeURIComponent(heroName)}`));
+
+
+/**
+ * Fetch and collate the data.
+ * (Do all the things!)
+ */
+
+async function fetchStats() {
+  const heroStats = await fetchAggregateStats()
+    .then(parseHeroAggregateHtml)
+    .catch(err => console.error('fetchAggregateStats', err));
+
+  const heroSkills = map(
+    skills => ({ skills }),
+    zipObj(
+      Object.keys(heroStats),
+      await Promise.all(
+        map(compose(
+          promise => {
+            return promise.then(parseHeroDetailHtml)
+              .catch(err => {
+                console.error('parseHeroDetailHtml:', err);
+                return [];
+              })
+          },
+          fetchDetailStats
+        ))(Object.keys(heroStats))
+      ).catch(err => console.error('heroDetailStats:', err))
+    )
+  );
+
+  // console.log('Hero stats:', heroStats);
+
+  // console.log('Hero skills:', heroSkills);
+
+  const heroStatsAndSkills = mergeWith(
+    merge,
+    heroStats,
+    heroSkills
+  );
+
+  // console.log('Hero stats and skills:', heroStatsAndSkills);
+
+  fs.writeFileSync('./lib/stats.json', JSON.stringify(heroStatsAndSkills, null, 2));
 }
 
-fetch('http://feheroes.wiki/Stats_Table')
-  .then(response => response.text())
-  .then(html => {
-    const heroRows = html.match(/<tr>((.|\n)*?)<\/tr>/g);
-    const heroes = mergeAll(tail(heroRows).map(parseHeroRow));
 
-    fs.writeFileSync('./lib/stats.json', JSON.stringify(heroes, null, 2));
-  });
+// TODO: Consume fetchStats in some more sophisticated scenario?
+fetchStats();
