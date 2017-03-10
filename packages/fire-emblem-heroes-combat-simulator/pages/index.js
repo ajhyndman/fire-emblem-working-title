08f9c1ff17c1bdd4575c6653d7d02b97850fd87e
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-10 23:23:57 +1100

    Extract HeroGrid and update styles

--- packages/fire-emblem-heroes-combat-simulator/pages/index.js
+++ packages/fire-emblem-heroes-combat-simulator/pages/index.js
@@ -2,41 +2,21 @@
 import Head from 'next/head'
 import React from 'react';
 import stats from 'fire-emblem-heroes-stats';
-import { map, range, replace } from 'ramda';
 
-import Hero from '../src/components/Hero';
+import HeroGrid from '../src/components/HeroGrid';
+
 
 const Home = () => (
-  <div>
+  <div className="root">
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style>{`
+        body {
+          background: #123346;
+        }
+      `}</style>
     </Head>
-    <style jsx>{`
-      .grid {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-      }
-      .grid:after {
-        content: "";
-        flex: auto;
-      }
-      div:empty {
-        margin: 0 5px;
-        width: 56px;
-      }
-    `}</style>
-    <div className="grid">
-      {map(
-        (hero) => <Hero key={hero.name} name={hero.name} weaponType={replace(' ', '_', hero.weaponType)} />,
-        stats.heroes,
-      )}
-      {/**
-        * Flexbox doesn't really provide grid support.  Add enough empty
-        * elements to preserve grid layout of last row on large screens.
-        */}
-      {map((i) => <div key={i} />, range(0, 30))}
-    </div>
+    <HeroGrid heroes={stats.heroes} />
   </div>
 );
 
--- packages/fire-emblem-heroes-combat-simulator/src/components/Hero.js
+++ packages/fire-emblem-heroes-combat-simulator/src/components/Hero.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 
+
 type Props = {
   name: string;
   weaponType: string;
@@ -10,19 +11,24 @@ const Hero = ({ name, weaponType }: Props) => (
   <div className="root">
     <style jsx>{`
       .root {
-        box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+        background: #5e7b8a;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+        cursor: pointer;
         margin: 5px;
+        position: relative;
         transition: box-shadow 0.2s;
       }
       .class {
+        left: -2px;
         position: absolute;
+        top: -2px;
       }
       .face {
         display: block;
         margin: 0 auto;
       }
       .root:hover {
-        box-shadow: 0 5px 35px rgba(0, 0, 0, 0.4);
+        box-shadow: 0 5px 30px rgba(0, 0, 0, 0.6);
       }
     `}</style>
     <img
