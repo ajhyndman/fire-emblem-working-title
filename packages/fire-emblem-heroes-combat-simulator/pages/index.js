a05f0ee1a366ab89c8b74ebafe2bab431313b84b
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-13 22:39:51 +1100

    Add favicon!

--- packages/fire-emblem-heroes-combat-simulator/pages/index.js
+++ packages/fire-emblem-heroes-combat-simulator/pages/index.js
@@ -67,6 +67,14 @@ class Home extends React.Component {
       <div className="root" onClick={() => dispatch({ type: 'SELECT_SLOT', slot: undefined })}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
+          <link rel="manifest" href="/static/manifest.json" />
+          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <meta name="msapplication-config" content="/static/browserconfig.xml" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             href="https://fonts.googleapis.com/css?family=Mandali&text=→×0123456789"
             rel="stylesheet"
