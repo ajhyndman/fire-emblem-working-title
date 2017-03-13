c60863151d02271266eee586cd8c725ec2227f02
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-14 00:24:05 +1100

    Add a title, for now
    Fixes #18

--- packages/fire-emblem-heroes-combat-simulator/pages/index.js
+++ packages/fire-emblem-heroes-combat-simulator/pages/index.js
@@ -67,6 +67,7 @@ class Home extends React.Component {
       <div className="root" onClick={() => dispatch({ type: 'SELECT_SLOT', slot: undefined })}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Proving Grounds — Fire Emblem: Heroes</title>
           <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
           <link rel="icon" type="image/png" href="/static/favicon-32x32.png" sizes="32x32" />
           <link rel="icon" type="image/png" href="/static/favicon-16x16.png" sizes="16x16" />
