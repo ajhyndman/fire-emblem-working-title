ee77d4402e74658d34934f4c1eb61a175569e911
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-20 00:33:07 +1100

    Add storybook typedef declaration

---
+++ packages/proving-grounds/flow/libs/storybook.js
@@ -0,0 +1,10 @@
+// @flow
+
+declare module '@kadira/storybook' {
+  declare interface Story {
+    add: (storyName: string, callback: Function) => Story;
+  }
+
+  declare function storiesOf(name: string, module: any): Story;
+  declare function action(name: string): Function;
+}
