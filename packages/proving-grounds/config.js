6f6aa824efbd465d33d8f47616362090b9515361
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-24 21:09:36 +1100

    Add docker build scripts, and make use of NODE_ENV

--- packages/proving-grounds/config.js
+++ packages/proving-grounds/config.js
@@ -1,3 +1,5 @@
 // @flow
 
-export const staticUrl = "https://s3-us-west-1.amazonaws.com/static.fire-emblem-combat-simulator/";
+export const staticUrl = process.env.NODE_ENV === 'production'
+  ? 'https://s3-us-west-1.amazonaws.com/static.fire-emblem-combat-simulator/'
+  : '/static/cdn/';
