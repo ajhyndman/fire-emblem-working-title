672a0bc7c8a11033d60fba56fa56c88863893e27
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-25 01:47:14 +1100

    Fix stat shape errors
    This'll be important for compatibility with hero configuration.
    
    Also run eslint --fix

--- packages/proving-grounds/src/temporal/2017.03.25-michalis.js
+++ packages/proving-grounds/src/temporal/2017.03.25-michalis.js
@@ -2,243 +2,233 @@
 import type { Hero } from 'fire-emblem-heroes-stats';
 
 const michalisUnits: Array<Hero> = [
-    {
-      "name": "Flier",
-      "imageName": "Lance Flier",
-      "moveType": "Flying",
-      "weaponType": "Blue Lance",
-      "skills": [
-        {
-          "name": "Silver Lance+",
-          "default": "-",
-          "rarity": "-"
+  {
+    "name": "Flier",
+    "imageName": "Lance Flier",
+    "moveType": "Flying",
+    "weaponType": "Blue Lance",
+    "skills": [
+      {
+        "name": "Silver Lance+",
+        "rarity": "-",
+      },
+      {
+        "name": "Iceberg",
+        "rarity": "-",
+      },
+      {
+        "name": "Darting Blow 3",
+        "rarity": "-",
+      },
+      {
+        "name": "Drag Back",
+        "rarity": "-",
+      },
+      {
+        "name": "Fortify Fliers",
+        "rarity": "-",
+      },
+    ],
+    "stats": {
+      "1": {
+        "5": {
+          "hp": "-",
+          "atk": "-",
+          "spd": "-",
+          "def": "-",
+          "res": "-",
         },
-        {
-          "name": "Iceberg",
-          "default": "-",
-          "rarity": "-"
+      },
+      "40": {
+        "5": {
+          "hp": [45],
+          "atk": [28],
+          "spd": [36],
+          "def": [22],
+          "res": [37],
         },
-        {
-          "name": "Darting Blow 3",
-          "rarity": "-"
-        },
-        {
-          "name": "Drag Back",
-          "rarity": "-"
-        },
-        {
-          "name": "Fortify Fliers",
-          "rarity": "-"
-        }
-      ],
-      "stats": {
-        "1": {
-          "5": {
-            "hp": ["-"],
-            "atk": ["-"],
-            "spd": ["-"],
-            "def": ["-"],
-            "res": ["-"]
-          }
-        },
-        "40": {
-          "5": {
-            "hp": [45],
-            "atk": [28],
-            "spd": [36],
-            "def": [22],
-            "res": [37]
-          }
-        }
-      }
+      },
     },
-    {
-      "name": "Michalis",
-      "moveType": "Flying",
-      "weaponType": "Green Axe",
-      "skills": [
-        {
-          "name": "Hauteclere",
-          "default": "-",
-          "rarity": "-"
+  },
+  {
+    "name": "Michalis",
+    "moveType": "Flying",
+    "weaponType": "Green Axe",
+    "skills": [
+      {
+        "name": "Hauteclere",
+        "rarity": "-",
+      },
+      {
+        "name": "Blazing Thunder",
+        "rarity": "-",
+      },
+      {
+        "name": "Iote's Shield",
+        "rarity": "-",
+      },
+      {
+        "name": "Threaten Def 3",
+        "rarity": "-",
+      },
+    ],
+    "stats": {
+      "1": {
+        "5": {
+          "hp": "-",
+          "atk": "-",
+          "spd": "-",
+          "def": "-",
+          "res": "-",
         },
-        {
-          "name": "Blazing Thunder",
-          "default": "-",
-          "rarity": "-"
+      },
+      "40": {
+        "5": {
+          "hp": [60],
+          "atk": [35],
+          "spd": [27],
+          "def": [36],
+          "res": [19],
         },
-        {
-          "name": "Iote's Shield",
-          "rarity": "-"
-        },
-        {
-          "name": "Threaten Def 3",
-          "rarity": "-"
-        }
-      ],
-      "stats": {
-        "1": {
-          "5": {
-            "hp": ["-"],
-            "atk": ["-"],
-            "spd": ["-"],
-            "def": ["-"],
-            "res": ["-"]
-          }
-        },
-        "40": {
-          "5": {
-            "hp": [60],
-            "atk": [35],
-            "spd": [27],
-            "def": [36],
-            "res": [19]
-          }
-        }
-      }
+      },
     },
-    {
-      "name": "Flier",
-      "imageName": "Sword Flier",
-      "moveType": "Flying",
-      "weaponType": "Red Sword",
-      "skills": [
-        {
-          "name": "Silver Sword+",
-          "default": "-",
-          "rarity": "-"
+  },
+  {
+    "name": "Flier",
+    "imageName": "Sword Flier",
+    "moveType": "Flying",
+    "weaponType": "Red Sword",
+    "skills": [
+      {
+        "name": "Silver Sword+",
+        "rarity": "-",
+      },
+      {
+        "name": "Iceberg",
+        "rarity": "-",
+      },
+      {
+        "name": "Darting Blow 3",
+        "rarity": "-",
+      },
+      {
+        "name": "Drag Back",
+        "rarity": "-",
+      },
+      {
+        "name": "Hone Fliers",
+        "rarity": "-",
+      },
+    ],
+    "stats": {
+      "1": {
+        "5": {
+          "hp": "-",
+          "atk": "-",
+          "spd": "-",
+          "def": "-",
+          "res": "-",
         },
-        {
-          "name": "Iceberg",
-          "default": "-",
-          "rarity": "-"
+      },
+      "40": {
+        "5": {
+          "hp": [45],
+          "atk": [28],
+          "spd": [36],
+          "def": [22],
+          "res": [37],
         },
-        {
-          "name": "Darting Blow 3",
-          "rarity": "-"
-        },
-        {
-          "name": "Drag Back",
-          "rarity": "-"
-        },
-        {
-          "name": "Hone Fliers",
-          "rarity": "-"
-        }
-      ],
-      "stats": {
-        "1": {
-          "5": {
-            "hp": ["-"],
-            "atk": ["-"],
-            "spd": ["-"],
-            "def": ["-"],
-            "res": ["-"]
-          }
-        },
-        "40": {
-          "5": {
-            "hp": [45],
-            "atk": [28],
-            "spd": [36],
-            "def": [22],
-            "res": [37]
-          }
-        }
-      }
+      },
     },
-    {
-      "name": "Cavalier",
-      "imageName": "Sword Cavalier",
-      "moveType": "Cavalry",
-      "weaponType": "Red Sword",
-      "skills": [
-        {
-          "name": "Silver Sword+",
-          "default": "-",
-          "rarity": "-"
+  },
+  {
+    "name": "Cavalier",
+    "imageName": "Sword Cavalier",
+    "moveType": "Cavalry",
+    "weaponType": "Red Sword",
+    "skills": [
+      {
+        "name": "Silver Sword+",
+        "rarity": "-",
+      },
+      {
+        "name": "Draconic Aura",
+        "rarity": "-",
+      },
+      {
+        "name": "Death Blow 3",
+        "rarity": "-",
+      },
+      {
+        "name": "Bowbreaker 3",
+        "rarity": "-",
+      },
+    ],
+    "stats": {
+      "1": {
+        "5": {
+          "hp": "-",
+          "atk": "-",
+          "spd": "-",
+          "def": "-",
+          "res": "-",
         },
-        {
-          "name": "Draconic Aura",
-          "default": "-",
-          "rarity": "-"
+      },
+      "40": {
+        "5": {
+          "hp": [49],
+          "atk": [35],
+          "spd": [25],
+          "def": [26],
+          "res": [30],
         },
-        {
-          "name": "Death Blow 3",
-          "rarity": "-"
-        },
-        {
-          "name": "Bowbreaker 3",
-          "rarity": "-"
-        }
-      ],
-      "stats": {
-        "1": {
-          "5": {
-            "hp": ["-"],
-            "atk": ["-"],
-            "spd": ["-"],
-            "def": ["-"],
-            "res": ["-"]
-          }
-        },
-        "40": {
-          "5": {
-            "hp": [49],
-            "atk": [35],
-            "spd": [25],
-            "def": [26],
-            "res": [30]
-          }
-        }
-      }
+      },
     },
-    {
-      "name": "Fighter",
-      "imageName": "Lance Fighter",
-      "moveType": "Infantry",
-      "weaponType": "Blue Lance",
-      "skills": [
-        {
-          "name": "Heavy Spear+",
-          "default": "-",
-          "rarity": "-"
+  },
+  {
+    "name": "Fighter",
+    "imageName": "Lance Fighter",
+    "moveType": "Infantry",
+    "weaponType": "Blue Lance",
+    "skills": [
+      {
+        "name": "Heavy Spear+",
+        "rarity": "-",
+      },
+      {
+        "name": "Draconic Aura",
+        "rarity": "-",
+      },
+      {
+        "name": "Death Blow 3",
+        "rarity": "-",
+      },
+      {
+        "name": "Bowbreaker 3",
+        "rarity": "-",
+      },
+    ],
+    "stats": {
+      "1": {
+        "5": {
+          "hp": "-",
+          "atk": "-",
+          "spd": "-",
+          "def": "-",
+          "res": "-",
         },
-        {
-          "name": "Draconic Aura",
-          "default": "-",
-          "rarity": "-"
+      },
+      "40": {
+        "5": {
+          "hp": [52],
+          "atk": [28],
+          "spd": [31],
+          "def": [30],
+          "res": [23],
         },
-        {
-          "name": "Death Blow 3",
-          "rarity": "-"
-        },
-        {
-          "name": "Bowbreaker 3",
-          "rarity": "-"
-        }
-      ],
-      "stats": {
-        "1": {
-          "5": {
-            "hp": ["-"],
-            "atk": ["-"],
-            "spd": ["-"],
-            "def": ["-"],
-            "res": ["-"]
-          }
-        },
-        "40": {
-          "5": {
-            "hp": [52],
-            "atk": [28],
-            "spd": [31],
-            "def": [30],
-            "res": [23]
-          }
-        }
-      }
-    }
-  ];
+      },
+    },
+  },
+];
 
 export default michalisUnits;
