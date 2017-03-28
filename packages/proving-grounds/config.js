4d969567d1c261aca765897d0d506c1905c1da70
Andrew Hyndman <ajhyndman@hotmail.com>
2017-03-28 20:27:25 +1100

    Configure Storybook to serve static files
    This means you can access storybook and the local site from other devices without breaking images.

--- packages/proving-grounds/config.js
+++ packages/proving-grounds/config.js
@@ -2,4 +2,4 @@
 
 export const staticUrl = process.env.NODE_ENV === 'production'
   ? 'https://s3-us-west-1.amazonaws.com/static.fire-emblem-combat-simulator/'
-  : 'http://localhost:3000/static/cdn/';
+  : '/static/cdn/';
