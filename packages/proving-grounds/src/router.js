dc614c33d821816589215ed60bb07fdbe06a5652
Andrew Hyndman <ajhyndman@hotmail.com>
2017-04-13 00:59:38 +1000

    Log first pageview to google analytics

--- packages/proving-grounds/src/router.js
+++ packages/proving-grounds/src/router.js
@@ -12,6 +12,7 @@ if (
   && window.location.hostname === 'proving-grounds.ajhyndman.com'
 ) {
   ReactGA.initialize('UA-97182834-1');
+  ReactGA.pageview(window.location.pathname);
   Router.onRouteChangeComplete = logPageView;
 }
 
