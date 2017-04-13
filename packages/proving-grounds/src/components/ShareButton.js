f7bb1acdd388472355baf1ba9d4b09a497ff36f3
Andrew Hyndman <ajhyndman@hotmail.com>
2017-04-14 02:46:17 +1000

    Revert "Add the word 'Share' below the icon"
    This reverts commit 5590a0c81963bb6e990e3c22602b42b3b7e7d23c.

--- packages/proving-grounds/src/components/ShareButton.js
+++ packages/proving-grounds/src/components/ShareButton.js
@@ -2,15 +2,8 @@
 import React from 'react';
 import clipboard from 'clipboard-js';
 import Link from 'react-icons/lib/go/link';
-import Color from 'color-js';
 
 import type { Dispatch } from '../reducer';
-import {
-  colors,
-  fontFamilies,
-  fontSizes,
-  lineHeights,
-} from '../theme';
 
 
 type Props = {
@@ -35,20 +28,6 @@ const ShareButton = ({ dispatch, link }: Props) => (
       button:focus {
         outline: none;
       }
-      .root {
-        position: relative;
-      }
-      .share-text {
-        color: ${Color(colors.iceberg).setAlpha(0.75)};
-        font-family: ${fontFamilies.ui};
-        font-size: ${fontSizes.small}px;
-        position: absolute;
-        left: 0;
-        bottom: -2px;
-        text-align: center;
-        text-shadow: 0.5px 1px 2px rgba(0, 0, 0, 0.8);
-        width: 100%;
-      }
     `}</style>
     <button
       onClick={() => {
@@ -60,7 +39,6 @@ const ShareButton = ({ dispatch, link }: Props) => (
       }}
     >
       <Link />
-      <div className="share-text">Share</div>
     </button>
   </div>
 );
