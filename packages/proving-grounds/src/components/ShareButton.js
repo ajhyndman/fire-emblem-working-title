5590a0c81963bb6e990e3c22602b42b3b7e7d23c
Matthew Hyndman <myndman@gmail.com>
2017-04-13 03:01:38 -0700

    Add the word 'Share' below the icon

--- packages/proving-grounds/src/components/ShareButton.js
+++ packages/proving-grounds/src/components/ShareButton.js
@@ -2,8 +2,15 @@
 import React from 'react';
 import clipboard from 'clipboard-js';
 import Link from 'react-icons/lib/go/link';
+import Color from 'color-js';
 
 import type { Dispatch } from '../reducer';
+import {
+  colors,
+  fontFamilies,
+  fontSizes,
+  lineHeights,
+} from '../theme';
 
 
 type Props = {
@@ -28,6 +35,20 @@ const ShareButton = ({ dispatch, link }: Props) => (
       button:focus {
         outline: none;
       }
+      .root {
+        position: relative;
+      }
+      .share-text {
+        color: ${Color(colors.iceberg).setAlpha(0.75)};
+        font-family: ${fontFamilies.ui};
+        font-size: ${fontSizes.small}px;
+        position: absolute;
+        left: 0;
+        bottom: -2px;
+        text-align: center;
+        text-shadow: 0.5px 1px 2px rgba(0, 0, 0, 0.8);
+        width: 100%;
+      }
     `}</style>
     <button
       onClick={() => {
@@ -39,6 +60,7 @@ const ShareButton = ({ dispatch, link }: Props) => (
       }}
     >
       <Link />
+      <div className="share-text">Share</div>
     </button>
   </div>
 );
