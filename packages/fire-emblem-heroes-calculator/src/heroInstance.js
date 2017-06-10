cb184e5b17ab215110a7f523724157a6df7c3c28
Andrew Hyndman <ajhyndman@hotmail.com>
2017-06-10 23:06:05 +1000

    Implement importInstance

--- packages/fire-emblem-heroes-calculator/src/heroInstance.js
+++ packages/fire-emblem-heroes-calculator/src/heroInstance.js
@@ -1,15 +1,17 @@
 // @flow
 // eslint
 import {
+  clamp,
   compose,
   equals,
   filter,
   join,
   map,
+  test,
   toPairs,
 } from 'ramda';
 
-import { getDefaultSkills } from './heroHelpers';
+import { getDefaultSkills, getInheritableSkills } from './heroHelpers';
 
 
 export type Stat = 'hp' | 'atk' | 'spd' | 'def' | 'res';
@@ -35,7 +37,7 @@ export type Buffs = {
   +res: number;
 };
 
-export type State = {
+export type InstanceState = {
   +hpMissing: number;
   +specialCharge: number;
   +buffs: Buffs;
@@ -50,7 +52,7 @@ export type HeroInstance = {
   +bane: ?Stat;
   +mergeLevel: MergeLevel;
   +skills: InstanceSkills;
-  +state: State;
+  +state: InstanceState;
 };
 
 export type Context = {
@@ -89,7 +91,7 @@ export const getDefaultBuffs = (): Buffs => ({
   res: 0,
 });
 
-const getDefaultState = (): State => ({
+const getDefaultState = (): InstanceState => ({
   hpMissing: 0,
   specialCharge: 0,
   buffs: getDefaultBuffs(),
@@ -115,8 +117,8 @@ export const getDefaultInstance = (name: string, rarity: Rarity = 5): HeroInstan
 });
 
 // eslint-disable-next-line no-undef
-const humanizeSkillSlot = (slot: $Keys<InstanceSkills>): string => {
-  switch (slot) {
+const humanizeSkillKey = (skillKey: $Keys<InstanceSkills>): string => {
+  switch (skillKey) {
     case 'WEAPON':
       return 'Weapon';
     case 'ASSIST':
@@ -136,11 +138,46 @@ const humanizeSkillSlot = (slot: $Keys<InstanceSkills>): string => {
   }
 };
 
+// eslint-disable-next-line no-undef
+const sanitizeSkillKey = (skillKey: ?string): ?$Keys<InstanceSkills> => {
+  switch (skillKey) {
+    case 'Weapon':
+      return 'WEAPON';
+    case 'Assist':
+      return 'ASSIST';
+    case 'Special':
+      return 'SPECIAL';
+    case 'A':
+      return 'PASSIVE_A';
+    case 'B':
+      return 'PASSIVE_B';
+    case 'C':
+      return 'PASSIVE_C';
+    case 'S':
+      return 'SEAL';
+    default:
+      return undefined;
+  }
+};
+
+const sanitizeStatKey = (statKey: ?string): ?Stat => {
+  switch (statKey) {
+    case 'hp':
+    case 'atk':
+    case 'spd':
+    case 'def':
+    case 'res':
+      return statKey;
+    default:
+      return undefined;
+  }
+};
+
 export const exportInstance = (heroInstance: HeroInstance): string => {
   // $FlowIssue: flow always gets lost in compose chains
   const skillList = compose(
     join('\n'),
-    map(([slot, skillName]) => `${humanizeSkillSlot(slot)}: ${skillName}`),
+    map(([slot, skillName]) => `${humanizeSkillKey(slot)}: ${skillName}`),
     filter(([, skillName]) => skillName !== undefined),
     toPairs,
   )(heroInstance.skills);
@@ -198,5 +235,109 @@ export const exportInstance = (heroInstance: HeroInstance): string => {
   }`;
 };
 
-// export const importInstance = (shareText: string): HeroInstance => {
-// };
+export const importInstance = (text: string): HeroInstance => {
+  // Lines that do not match one of these regular expressions will be ignored.
+  const titleRegex = /^(.*) \((\d)★\+?(\d+)?(?: \+)?([a-z]+)?(?: \-)?([a-z]+)?\)\s*/;
+  const skillRegex = /^(Weapon|Assist|Special|A|B|C|S): (.*?)\s*$/;
+  const buffRegex = /^Buffs:\s*(.*?)\s*$/;
+  const debuffRegex = /^Debuffs:\s*(.*?)\s*$/;
+  const damageRegex = /^Damage:\s*(\d*)\s*$/;
+  const chargeRegex = /^Charge:\s*(\d*)\s*$/;
+
+  // Initialize components of the hero instance we will build up.
+  let name = 'Anna';
+  let rarity = 0;
+  let mergeLevel = 0;
+  let boon = undefined;
+  let bane = undefined;
+  const skills: InstanceSkills = {
+    WEAPON: undefined,
+    ASSIST: undefined,
+    SPECIAL: undefined,
+    PASSIVE_A: undefined,
+    PASSIVE_B: undefined,
+    PASSIVE_C: undefined,
+    SEAL: undefined,
+  };
+  const state: InstanceState = getDefaultState();
+
+  // Split the input text into lines.
+  const lines = text.split(/\n|;/);
+
+  // Attempt to parse each line.
+  for (let line of lines) {
+    // If a line matches one of our regular expressions, apply it to the instance.
+    // Otherwise, ignore it.
+    if (test(titleRegex, line)) {
+      // e.g. "Anna (4★+8 +atk -def)"
+
+      [, name, rarity, mergeLevel, boon, bane] = line.match(titleRegex);
+    } else if (test(skillRegex, line)) {
+      // e.g. "Weapon: Silver Axe"
+
+      const [, skillKey, skillName] = line.match(skillRegex);
+      if (
+        sanitizeSkillKey(skillKey)
+        // $FlowIssue: Flow is worried about side effects here (but there aren't any)
+        && getInheritableSkills(name, sanitizeSkillKey(skillKey)).indexOf(skillName) >= 0
+      ) {
+        // $FlowIssue: Flow doesn't have enough info to be sure that this is covariant-safe
+        skills[sanitizeSkillKey(skillKey)] = skillName;
+      }
+    } else if (test(buffRegex, line)) {
+      // e.g. "Buffs: atk 4, spd 4, def 8"
+
+      const [, buffList] = line.match(buffRegex);
+      const buffs = buffList.split(/,\s*/);
+      buffs.forEach(buff => {
+        const [statKey, value] = buff.split(' ');
+        if (sanitizeStatKey(statKey)) {
+          // $FlowIssue: Flow doesn't have enough info to be sure that this is covariant-safe
+          state.buffs[sanitizeStatKey(statKey)] = parseInt(value) || 0;
+        }
+      });
+    } else if (test(debuffRegex, line)) {
+      // e.g. Debuffs: atk -3, spd -3, res -3
+
+      const [, debuffList] = line.match(debuffRegex);
+      const debuffs = debuffList.split(/,\s*/);
+      debuffs.forEach(buff => {
+        const [statKey, value] = buff.split(' ');
+        if (sanitizeStatKey(statKey)) {
+          // $FlowIssue: Flow doesn't have enough info to be sure that this is covariant-safe
+          state.debuffs[sanitizeStatKey(statKey)] = Math.abs(parseInt(value));
+        }
+      });
+    } else if (test(damageRegex, line)) {
+      // e.g. "Damage: 5"
+
+      const [, value] = line.match(damageRegex);
+      // $FlowIssue: Flow doesn't have enough info to be sure that this is covariant-safe
+      state.hpMissing = Math.max(0, parseInt(value));
+    } else if (test(chargeRegex, line)) {
+      // e.g. "Charge: 1"
+
+      const [, value] = line.match(chargeRegex);
+      // $FlowIssue: Flow doesn't have enough info to be sure that this is covariant-safe
+      state.specialCharge = Math.max(0, parseInt(value));
+    }
+  }
+
+  // $FlowIssue: Flow can't do the math to see that this is satisfied
+  rarity = (clamp(1, 5, parseInt(rarity)): Rarity);
+  // $FlowIssue: Flow can't do the math to see that this is satisfied
+  mergeLevel = (clamp(0, 10, parseInt(mergeLevel) || 0): MergeLevel);
+
+  bane = sanitizeStatKey(bane);
+  boon = sanitizeStatKey(boon);
+
+  return {
+    name,
+    rarity,
+    mergeLevel,
+    boon,
+    bane,
+    skills,
+    state,
+  };
+};
--- packages/fire-emblem-heroes-calculator/tests/heroInstanceTest.js
+++ packages/fire-emblem-heroes-calculator/tests/heroInstanceTest.js
@@ -4,9 +4,10 @@ import test from 'tape';
 import {
   exportInstance,
   getDefaultInstance,
-  // importInstance,
+  importInstance,
 } from '../src/heroInstance';
 import { getDefaultSkills } from '../src/heroHelpers';
+
 
 test('exportInstance', (t) => {
   t.plan(4);
@@ -25,16 +26,15 @@ C: Spur Res 3
   // with hero variance
   t.equal(
     exportInstance({
-      ...getDefaultInstance('Anna'),
-      rarity: 4,
+      ...getDefaultInstance('Anna', 4),
       mergeLevel: 8,
       boon: 'atk',
       bane: 'def',
     }),
     `Anna (4★+8 +atk -def)
-Weapon: Nóatún
+Weapon: Silver Axe
 Special: Astra
-B: Vantage 3
+B: Vantage 2
 C: Spur Res 3
 `,
   );
@@ -95,5 +95,94 @@ Debuffs: atk -3, spd -3, res -3
 Damage: 5
 Charge: 1
 `,
+  );
+});
+
+test('importInstance', (t) => {
+  t.plan(4);
+
+  // default instance
+  t.deepEqual(
+    importInstance(`Anna (5★)
+Weapon: Nóatún
+Special: Astra
+B: Vantage 3
+C: Spur Res 3
+`),
+    getDefaultInstance('Anna'),
+  );
+
+  // with hero variance
+  t.deepEqual(
+    importInstance(`Anna (4★+8 +atk -def)
+Weapon: Silver Axe
+Special: Astra
+B: Vantage 2
+C: Spur Res 3
+`),
+    {
+      ...getDefaultInstance('Anna', 4),
+      mergeLevel: 8,
+      boon: 'atk',
+      bane: 'def',
+    },
+  );
+
+  // with all skills
+  t.deepEqual(
+    importInstance(`Anna (5★)
+Weapon: Nóatún
+Assist: Reposition
+Special: Astra
+A: Fury 3
+B: Vantage 3
+C: Spur Res 3
+S: HP +3
+`),
+    {
+      ...getDefaultInstance('Anna'),
+      skills: {
+        ...getDefaultSkills('Anna'),
+        ASSIST: 'Reposition',
+        PASSIVE_A: 'Fury 3',
+        SEAL: 'HP +3',
+      },
+    },
+  );
+
+  // with status effects
+  t.deepEqual(
+    importInstance(`Anna (5★)
+Weapon: Nóatún
+Special: Astra
+B: Vantage 3
+C: Spur Res 3
+:::Status
+Buffs: atk 4, spd 4, def 8
+Debuffs: atk -3, spd -3, res -3
+Damage: 5
+Charge: 1
+`),
+    {
+      ...getDefaultInstance('Anna'),
+      state: {
+        hpMissing: 5,
+        specialCharge: 1,
+        buffs: {
+          hp: 0,
+          atk: 4,
+          spd: 4,
+          def: 8,
+          res: 0,
+        },
+        debuffs: {
+          hp: 0,
+          atk: 3,
+          spd: 3,
+          def: 0,
+          res: 3,
+        },
+      },
+    },
   );
 });
