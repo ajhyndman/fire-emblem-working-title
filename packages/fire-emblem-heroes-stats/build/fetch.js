ed3c6ef5dadb661d62118a1d7b4a05d404611909
Andrew Hyndman <ajhyndman@hotmail.com>
2017-07-30 13:54:07 +1000

    Add fetchImage abstraction

--- packages/fire-emblem-heroes-stats/build/fetch.js
+++ packages/fire-emblem-heroes-stats/build/fetch.js
@@ -1,8 +1,14 @@
 import fetch from 'isomorphic-fetch';
+import fs from 'fs';
+import path from 'path';
+import { URL } from 'url';
 import {
   compose,
+  last,
   map,
+  prop,
   replace,
+  split,
   zipObj,
 } from 'ramda';
 
@@ -41,3 +47,25 @@ export async function fetchAndParsePages(host, pageNames, parseFunction) {
     ).catch(err => console.error('fetchAndParsePages:', err)),
   );
 }
+
+/**
+ * Download an image and save it into assets, with the same name.
+ */
+export const fetchImage = (url) =>
+  fetch(url)
+    .then(response => {
+      if (!response.ok) return Promise.reject({ type: '404' });
+
+      // extract a filename with a consistent naming scheme from our url
+      const fileName = compose(
+        replace(/_/g, (match, offset) => (offset < 20 ? match : ' ')),
+        decodeURIComponent,
+        last,
+        split('/'),
+        prop('pathname'),
+        url => new URL(url),
+      )(url);
+
+      const file = fs.createWriteStream(path.join(__dirname, `../assets/${fileName}`));
+      response.body.pipe(file);
+    })
