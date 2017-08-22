de200112445e3ebdb234a05d388b262b13f2f2ca
Andrew Hyndman <ajhyndman@hotmail.com>
2017-08-23 00:11:25 +1000

    Update wiki host with https protocol

--- packages/fire-emblem-heroes-stats/build/constants.js
+++ packages/fire-emblem-heroes-stats/build/constants.js
@@ -1,2 +1,2 @@
 export const CDN_HOST = 'https://proving-grounds-static.ajhyndman.com';
-export const WIKI_HOST = 'http://feheroes.gamepedia.com';
+export const WIKI_HOST = 'https://feheroes.gamepedia.com';
